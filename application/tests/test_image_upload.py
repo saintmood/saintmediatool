d213b0dcae3524e1712c68e2fbed050edd68af1c
Jimm <saintmoodjohnny@gmail.com>
2021-01-08 00:44:34 +0200

    extend upload image testcase.

--- application/tests/test_image_upload.py
+++ application/tests/test_image_upload.py
@@ -6,7 +6,12 @@ class ImageUploadTestCase(BaseTestCase):
     endpoint_url = '/upload/images/'
 
     def test_upload_image_success(self):
-        response = self.client.post(self.endpoint_url, headers={"AuthToken": "sometokenvalue"})
+        upload_data = {
+            'image': 'image_object'
+        }
+        expected_url = 'http://saintmtool/media/pictures/picture_id'
+        response = self.client.post(self.endpoint_url, headers={"AuthToken": "sometokenvalue"}, json=upload_data)
         self.assertEqual(response.status_code, 201)
         resp_json = response.json()
         self.assertEqual(resp_json['status'], 'success')
+        self.assertEqual(resp_json['url'], expected_url)
