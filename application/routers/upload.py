7c49f1dcac8343b6302adc09256bf944b790e7b7
Jimm <saintmoodjohnny@gmail.com>
2021-01-13 23:22:44 +0200

    add mock to upload file to s3

--- application/routers/upload.py
+++ application/routers/upload.py
@@ -1,11 +1,17 @@
-from fastapi import APIRouter, File, UploadFile
+import boto3
+from botocore.exceptions import ClientError
+from fastapi import APIRouter, Depends, File, UploadFile
 
+from ..settings import settings
 
-router = APIRouter(prefix='/upload')
+router = APIRouter(prefix='/upload', dependencies=[Depends(settings)])
 
 
 @router.post('/images/', status_code=201)
 async def upload_image(
     upload:UploadFile=File(...)
 ):
+    s3_client = boto3.client('s3')
+    bucket_name = 'saintmtool'
+    s3_client.upload_fileobj(upload.file, bucket_name, upload.filename)
     return {'status': 'success', 'url': 'http://saintmtool/media/pictures/picture_id'}
--- application/tests/test_image_upload.py
+++ application/tests/test_image_upload.py
@@ -1,3 +1,6 @@
+import boto3
+from moto import mock_s3
+
 from application.tests import fixtures
 from .base import BaseTestCase
 
@@ -6,13 +9,16 @@ class ImageUploadTestCase(BaseTestCase):
 
     endpoint_url = '/upload/images/'
 
+    @mock_s3
     def test_upload_image_success(self):
+        conn = boto3.resource('s3')
+        conn.create_bucket(Bucket='saintmtool')
         test_image = fixtures.create_test_image()
         expected_url = 'http://saintmtool/media/pictures/picture_id'
         response = self.client.post(
             self.endpoint_url, 
             headers={'AuthToken': 'sometokenvalue'},
-            files={'upload': ('filename', test_image, 'image/png')}
+            files={'upload': (test_image.name, test_image, 'image/png')}
         )
         self.assertEqual(response.status_code, 201)
         resp_json = response.json()
